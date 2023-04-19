60991ac81b54c98fb770dac3359bee7941896755
Jack Hsu <jack.hsu@gmail.com>
2023-04-19 10:19:53 -0400

    add storybook generate script

--- add-storybook.sh
+++ add-storybook.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
 
 cd tmp
+for dir in */ ; do
+  cd $dir
+
+  next_version=$(cat package.json | jq ".dependencies.next ")
+
+  if [ "$next_version" != "null" ]; then
+    echo "Adding Storybook to Next.js workspace: $dir"
+    npx nx g @nrwl/react:component Message --project=demo
+    npx nx g @nrwl/react:storybook-configuration --generateStories --no-interactive
+  else 
+    echo "Adding Storybook to React workspace: $dir"
+    npx nx g @nrwl/react:component Message
+    npx nx g @nrwl/react:storybook-configuration --generateStories --no-interactive
+  fi
+
+  cd ..
+done
+
 
