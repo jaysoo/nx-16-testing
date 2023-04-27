eac311357119bc944af8757f3f65cc41f32f7517
Jack Hsu <jack.hsu@gmail.com>
2023-04-27 09:45:11 -0400

    fix node e2e setup

--- create.sh
+++ create.sh
@@ -8,12 +8,16 @@ ver="${1:-latest}"
 
 echo "Create workspaces with Nx $ver"
 
+npx -y -q create-nx-workspace@$ver node-cli --preset=node-standalone --framework=none --no-nx-cloud
 npx -y -q create-nx-workspace@$ver node-fastify --preset=node-standalone --framework=fastify --no-nx-cloud --docker
+npx -y -q create-nx-workspace@$ver node-nest --preset=node-standalone --framework=nest --no-nx-cloud --docker
+npx -y -q create-nx-workspace@$ver node-express --preset=node-standalone --framework=express --no-nx-cloud --docker
+npx -y -q create-nx-workspace@$ver node-koa --preset=node-standalone --framework=koa --no-nx-cloud --docker
 npx -y -q create-nx-workspace@$ver react-vite --preset=react-standalone --style=css --no-nx-cloud --bundler=vite
 npx -y -q create-nx-workspace@$ver react-webpack --preset=react-standalone --style=css --no-nx-cloud --bundler=webpack
 npx -y -q create-nx-workspace@$ver next-css --preset=next --appName=demo --style=css --no-nx-cloud --bundler=webpack
 npx -y -q create-nx-workspace@$ver next-scss --preset=next --appName=demo --style=scss --no-nx-cloud --bundler=webpack
 npx -y -q create-nx-workspace@$ver next-less --preset=next --appName=demo --style=less --no-nx-cloud --bundler=webpack
 npx -y -q create-nx-workspace@$ver next-styled-components --preset=next --appName=demo --style=styled-components --no-nx-cloud --bundler=webpack
-npx -y -q create-nx-workspace@$ver next-emotion --preset=next --appName=demo --style=@emotion/styled --no-nx-cloud --bundler=webpack
 npx -y -q create-nx-workspace@$ver next-styled-jsx --preset=next --appName=demo --style=styled-jsx --no-nx-cloud --bundler=webpack
+npx -y -q create-nx-workspace@$ver next-emotion --preset=next --appName=demo --style=@emotion/styled --no-nx-cloud --bundler=webpack
--- test-e2e.sh
+++ test-e2e.sh
@@ -3,23 +3,31 @@ export $(cat .env | xargs)
 
 cd $TMP
 
+PORT=3000
+export PORT
+
 for dir in * ; do
   cd $dir
 
-  fastify_version=$(cat package.json | jq ".dependencies.fastify ")
-  if [ "$fastify_version" != "null" ]; then
-    npx nx serve&
-    pid=$!
+  nx_node_version=$(cat package.json | jq '.devDependencies|.["@nx/node"]')
+  nrwl_node_version=$(cat package.json | jq '.devDependencies|.["@nrwl/node"]')
+  if [ "$nx_node_version" != "null" ] || [ "$nrwl_node_version" != "null" ]; then
+    npx nx serve --verbose&
+    sleep 3
     npx nx run-many -t=e2e --parallel=1
     ret=$?
-    kill $pid
+    lsof -i:$PORT | grep node | awk '{print $2}' | xargs kill
+    sleep 2
     if [ $ret -ne 0 ]; then
       echo "e2e failed for $dir"
       exit 1
     fi
-else
+    PORT=$((PORT+1))  
+    export PORT
+  else
     npx nx run-many -t=e2e --parallel=1 || { echo "e2e failed for $dir"; exit 1; }
   fi
+
   cd ..
 done
 
